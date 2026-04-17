616c8c10e5f649e12353e662e3c22cd681e1977b
abdullahEds <abdullah@edsglobal.com>
2026-04-17 09:57:07 +0530

    Dockerfile added

--- report_api.py
+++ report_api.py
@@ -30,11 +30,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    allow_origins=["*"],  # Allow all origins for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
